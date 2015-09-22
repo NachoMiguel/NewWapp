ba522cd3c75248a8f553ed62d0feb2bb7b53d8cc
Ignacio <ariel.x.vazquez.-nd@disney.com>
2015-09-22 14:53:49 -0300

    bastante mejorada la ui. Hacer que el proceso de datos se aplique en la misma ui

--- wappApp/static/wappApp/js/main.js
+++ wappApp/static/wappApp/js/main.js
@@ -1,120 +1,44 @@
-/*
-	Strata by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+jQuery(function($) {
 
-(function($) {
-
-	var settings = {
-
-		// Parallax background effect?
-			parallax: true,
-
-		// Parallax factor (lower = more intense, higher = less intense).
-			parallaxFactor: 20
-
-	};
-
-	skel.breakpoints({
-		xlarge: '(max-width: 1800px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
+	//Preloader
+	var preloader = $('.preloader');
+	$(window).load(function(){
+		preloader.remove();
 	});
 
-	$(function() {
+	// Menu	
+	$('#header .nav-button').on('click',function(){
+		$('#navigation').fadeIn();
+	});
 
-		var $window = $(window),
-			$body = $('body'),
-			$header = $('#header');
+	$('#hidemenu').on('click', function(){
+		$('#navigation').fadeOut();
+	});
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+	$('.main-nav ul li a').on('click', function(){
+		$('#navigation').fadeOut();
+	});
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+	var slider = $('#page-slider .carousel-inner').find('.item');
+	$('#page-slider').on('slid.bs.carousel', function () {
+		var curIndex 	= slider.filter('.active').index();
+		var menuItems 	= $('.main-nav ul').find('li');
+		menuItems.removeClass('active').eq(curIndex).addClass('active');
+	});
 
-		// Touch?
-			if (skel.vars.mobile) {
-
-				// Turn on touch mode.
-					$body.addClass('is-touch');
-
-				// Height fix (mostly for iOS).
-					window.setTimeout(function() {
-						$window.scrollTop($window.scrollTop() + 1);
-					}, 0);
-
+	//Contact Form
+	var form = $('#contact-form');
+	form.submit(function(event){
+		event.preventDefault();
+		var form_status = $('.form-status');
+		$.ajax({
+			url: $(this).attr('action'),
+			beforeSend: function(){
+				form_status.find('.form-status-content').html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn();
 			}
-
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
-
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
-
-		// Header.
-
-			// Parallax background.
-
-				// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-					if (skel.vars.browser == 'ie'
-					||	skel.vars.mobile)
-						settings.parallax = false;
-
-				if (settings.parallax) {
-
-					skel.on('change', function() {
-
-						if (skel.breakpoint('medium').active) {
-
-							$window.off('scroll.strata_parallax');
-							$header.css('background-position', 'top left, center center');
-
-						}
-						else {
-
-							$header.css('background-position', 'left 0px');
-
-							$window.on('scroll.strata_parallax', function() {
-								$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-							});
-
-						}
-
-					});
-
-				}
-
-		// Main Sections: Two.
-
-			// Lightbox gallery.
-				$window.on('load', function() {
-
-					$('#two').poptrox({
-						caption: function($a) { return $a.next('h3').text(); },
-						overlayColor: '#2c2c2c',
-						overlayOpacity: 0.85,
-						popupCloserText: '',
-						popupLoaderText: '',
-						selector: '.work-item a.image',
-						usePopupCaption: true,
-						usePopupDefaultStyling: false,
-						usePopupEasyClose: false,
-						usePopupNav: true,
-						windowMargin: (skel.breakpoint('small').active ? 0 : 50)
-					});
-
-				});
-
+		}).done(function(data){
+			form_status.find('.form-status-content').html('<p class="text-success">Thank you for contact us. As early as possible  we will contact you</p>').delay(3000).fadeOut();
+		});
 	});
 
-})(jQuery);
+});
